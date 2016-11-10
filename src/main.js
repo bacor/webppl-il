98dbc67efdf4c48ca990422a9846c3731258b511
bacor <bjmcornelissen@gmail.com>
2016-11-10 22:06:44 +0100

    Kalish&Griffiths

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-module.exports = {
+var _ = require('underscore');
 
-	
-	
+module.exports = {
+	call: function(fn, args) {
+		var _fn = fn;
+		var x = _fn(2)
+		return x
+		return _fn(args);
+	}
 };
